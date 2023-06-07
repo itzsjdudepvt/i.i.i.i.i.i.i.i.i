f10b9ad6b677eacddd20852e4fc4f675b2c02e87
itzsjdudepvt <134954045+itzsjdudepvt@users.noreply.github.com>
2023-06-07 17:20:33 +0530

    Update __main__.py

--- infinix/__main__.py
+++ infinix/__main__.py
@@ -4,6 +4,8 @@ from infinix.utils import load_plug
 import logging 
 from . import bot,bot1,bot2,bot3,bot4,bot5
 
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',level=logging.WARNING)
+
 path= "infinix/plugins/*.py"
 files=glob.glob(path)
 
@@ -13,20 +15,18 @@ for name in files:
     plug_name = pxt.stem
     load_plug(plug_name.replace(".py", ""))
     
-bot1.start()
-bot2.start()
-bot3.start()
-bot4.start()
-bot5.start()
+try:    
+  bot1.start()
+  bot2.start()
+  bot3.start()
+  bot4.start()
+  bot5.start()
+except:
+  break
 print("ClBot_started")
 
 if __name__ == "__main__":
   
-  bot1.run_until_disconnected()
-  bot2.run_until_disconnected()
-  bot3.run_until_disconnected()
-  bot4.run_until_disconnected()
-  bot5.run_until_disconnected()
   bot.run_until_disconnected()
 
 
