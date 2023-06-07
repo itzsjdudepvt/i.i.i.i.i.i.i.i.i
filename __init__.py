8ca157187ef668fb9783293474f0c352540709f5
itzsjdudepvt <134954045+itzsjdudepvt@users.noreply.github.com>
2023-06-07 13:38:54 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,24 @@
+from telethon import TelegramClient
+from telethon.sessions import StringSesssion 
+import logging
+import time
+ 
+  
+api_id = os.environ.get("api_id", None)
+api_hash = os.environ.get("api_hash", None)
+bot_token = os.environ.get("bot_token", None)
+str1 = os.environ.get("str1", None)
+str2 = os.environ.get("str2", None)
+str3 = os.environ.get("str3", None)
+str4 = os.environ.get("str4", None)
+str5 = os.environ.get("str5", None)
+
+
+
+bot = TelegramClient("my.b.o.t", api_id, api_hash).start(bot_token=bot_token)
+bot1 = TelegramClient((StringSession(str1)),api_id, api_hash)
+bot2 = TelegramClient((StringSession(str2)),api_id, api_hash)
+bot3 = TelegramClient((StringSession(str3)),api_id, api_hash)
+bot4 = TelegramClient((StringSession(str4)),api_id, api_hash)
+bot5 = TelegramClient((StringSession(str5)),api_id, api_hash)
+
