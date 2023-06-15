6640a2b20f0201eda474036b28bbf81e8bf135eb
itzsjdudepvt <134954045+itzsjdudepvt@users.noreply.github.com>
2023-06-15 06:51:28 +0530

    Update __main__.py

--- infinix/__main__.py
+++ infinix/__main__.py
@@ -5,7 +5,7 @@ import logging
 from logging import getLogger
 import asyncio
 from . import bot,bot1,bot2,bot3,bot4,bot5
- 
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level= logging.WARNING)
 path= "infinix/plugins/*.py"
 files=glob.glob(path)
 
