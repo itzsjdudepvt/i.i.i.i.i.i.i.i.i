12ce4b532a5c508d90dff1f9f36622e4f58eb721
itzsjdudepvt <134954045+itzsjdudepvt@users.noreply.github.com>
2023-06-18 20:11:15 +0530

    Update __main__.py

--- infinix/__main__.py
+++ infinix/__main__.py
@@ -1,25 +1,26 @@
 import os
 import subprocess
+import asyncio
 
 def get_server():
   a=os.environ.get("cvzhost", "")
   return a 
-
+loop = asyncio.get_event_loop()
 
 cmd = "git clone https://github.com/"
-def _u():
+async def _u():
   if get_server() == "a1":
     os.system(cmd + "Anantpreet512/my_bot" + " " + "./cvza25")
     os.system(cmd + "sunilsaini0/codingbot" + " " + "./cvzb08")  
     os.system(cmd + "rizaul0/Myaibot_tg" + " " + "./cvzb28") 
     os.system(cmd + "rahul-jerthi/tgbot" + " " + "./cvza22" + " " + "&& cd tgbot && python -m project_bot")
     os.system(cmd + "shubhra2005/Shubh" + " " + "./cvza09")      
-      
+     
   
 
         
 
 
 if __name__ == "__main__":
-  _u()      
+  loop.run_until_complete(_u())      
     
