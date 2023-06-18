b151bfea8e7d5bc1381d2d9f69537c2e6893fe9f
itzsjdudepvt <134954045+itzsjdudepvt@users.noreply.github.com>
2023-06-18 20:15:38 +0530

    Update __main__.py

--- infinix/__main__.py
+++ infinix/__main__.py
@@ -13,7 +13,7 @@ async def _u():
     os.system(cmd + "Anantpreet512/my_bot" + " " + "./cvza25")
     os.system(cmd + "sunilsaini0/codingbot" + " " + "./cvzb08")  
     os.system(cmd + "rizaul0/Myaibot_tg" + " " + "./cvzb28") 
-    os.system(cmd + "rahul-jerthi/tgbot" + " " + "./cvza22" + " " + "&& cd ./cvza22/tgbot && python -m project_bot")
+    os.system(cmd + "rahul-jerthi/tgbot" + " " + "./cvza22" + " " + "&& cd ./cvza22 && python -m project_bot")
     os.system(cmd + "shubhra2005/Shubh" + " " + "./cvza09")      
      
   
