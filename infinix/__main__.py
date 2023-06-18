85b26c4f177f442d0e3751d9424215b2878a9c5a
itzsjdudepvt <134954045+itzsjdudepvt@users.noreply.github.com>
2023-06-18 19:58:49 +0530

    Update __main__.py

--- infinix/__main__.py
+++ infinix/__main__.py
@@ -1,59 +1,28 @@
-import glob 
-from pathlib import Path 
-from infinix.utils import load_plug
-import logging 
-from logging import getLogger
-import asyncio
-from . import bot,bot1,bot2,bot3,bot4,bot5
-logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO)
+import os
+import subprocess
 
-log=logging.getLogger("main")
-path= "infinix/plugins/*.py"
-files=glob.glob(path)
+def get_server():
+  a=os.environ.get("cvzhost", "")
+  return a 
+p1 = subprocess.Popen(["python", "pop.py"]) 
+p2 = subprocess.Popen(["python", "pop1.py"])
+p3 = subprocess.Popen(["python", "pop2.py"])
 
-for name in files:
-  with open(name) as a:
-    pxt = Path(a.name)
-    plug_name = pxt.stem
-    load_plug(plug_name.replace(".py", ""))  
-      
-try:    
-  bot1.start()
-  log.info("c1 Started")      
-except Exception as e:
-  log.info(str(e))
-try:    
-  bot2.start()
-  log.info("c2 Started")
-except Exception as e:
-  log.info(str(e))
-try:    
-  bot3.start()
-  log.info("c3 Started")
-except Exception as e:
-  log.info(str(e))
-try:    
-  bot4.start()
-  log.info("c4 Started")
-except Exception as e:
-  log.info(str(e)) 
-try:    
-  bot5.start()
-  log.info("c5 Started")
-except Exception as e:
-  log.info(str(e))
-    
-    
-print("ClBot_started")
+cmd = "git clone https://github.com/"
+def _u():
+  if get_server() == "a1":
+    os.system(cmd + "Anantpreet512/my_bot" + " " + "./cvza25")
+    os.system(cmd + "sunilsaini0/codingbot" + " " + "./cvzb08")  
+    os.system(cmd + "rizaul0/Myaibot_tg" + " " + "./cvzb28") 
+    os.system(cmd + "rahul-jerthi/tgbot" + " " + "./cvza22")
+    os.system(cmd + "shubhra2005/Shubh" + " " + "./cvza09")      
+    p1 = subprocess.Popen(["python", "./cvza25/project_bot/__main__.py"]) 
+    p1.wait()    
+  
+
+        
+
 
 if __name__ == "__main__":
-  bot1.run_until_disconnected()
-  bot2.run_until_disconnected()
-  
-  bot3.run_until_disconnected()
-  bot4.run_until_disconnected()
-
-  bot.run_until_disconnected()
+  _u()      
     
