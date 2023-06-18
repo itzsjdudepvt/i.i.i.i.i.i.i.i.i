1bb7a8dec128d4e12c1947fc622934f3d5238688
itzsjdudepvt <134954045+itzsjdudepvt@users.noreply.github.com>
2023-06-18 20:57:40 +0530

    Update __main__.py

--- infinix/__main__.py
+++ infinix/__main__.py
@@ -10,16 +10,16 @@ loop = asyncio.get_event_loop()
 cmd = "git clone https://github.com/"
 async def _u():
   if get_server() == "a1":
+    from subprocess import Popen
+    Popen(cmd + "Anantpreet512/my_bot" + " " + "./cvza25" + " " + "&& cd ./cvza25 && python3 -m INFINITY",  shell=True )
     
-    os.system(cmd + "Anantpreet512/my_bot" + " " + "./cvza25" + " " + "&& cd ./cvza25 && python3 -m INFINITY" )
-    
-    os.system(cmd + "sunilsaini0/codingbot" + " " + "./cvzb08"  + " " + "&& cd ./cvzb08 && python3 -m coding" )
+    Popen(cmd + "sunilsaini0/codingbot" + " " + "./cvzb08"  + " " + "&& cd ./cvzb08 && python3 -m coding", shell=True )
      
-    os.system(cmd + "rizaul0/Myaibot_tg" + " " + "./cvzb28"  + " " + "&& cd ./cvzb28 && python3 -m infinix" ) 
+    Popen(cmd + "rizaul0/Myaibot_tg" + " " + "./cvzb28"  + " " + "&& cd ./cvzb28 && python3 -m infinix", shell=True ) 
    
-    os.system(cmd + "rahul-jerthi/tgbot" + " " + "./cvza22" + " " + "&& cd ./cvza22 && python3 -m project_bot")
+    Popen(cmd + "rahul-jerthi/tgbot" + " " + "./cvza22" + " " + "&& cd ./cvza22 && python3 -m project_bot", shell=True)
   
-    os.system(cmd + "shubhra2005/Shubh" + " " + "./cvza09")
+   
    
   
 
