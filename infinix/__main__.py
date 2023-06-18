1d2f030e79319ab744bfc6ac3d235a0163900efc
itzsjdudepvt <134954045+itzsjdudepvt@users.noreply.github.com>
2023-06-18 21:52:46 +0530

    Update __main__.py

--- infinix/__main__.py
+++ infinix/__main__.py
@@ -10,18 +10,15 @@ loop = asyncio.get_event_loop()
 cmd = "git clone https://github.com/"
 async def _u():
   if get_server() == "a1":
-    from subprocess import Popen
-    try:
-      ax=Popen(cmd + "Anantpreet512/my_bot" + " " + "./cvza25" + " " + "&& cd ./cvza25 && python3 -m INFINITY",  shell=True )
-      ax.wait()
-    except subprocess.CalledProcessError as e:
-      print(e)
+    os.system(cmd + "Anantpreet512/my_bot" + " " + "./cvza25" + " " + "&& cd ./cvza25 && python3 -m INFINITY &" )
     
-    bx=Popen(cmd + "sunilsaini0/codingbot" + " " + "./cvzb08"  + " " + "&& cd ./cvzb08 && python3 -m coding", shell=True )
-    bx.wait() 
-    Popen(cmd + "rizaul0/Myaibot_tg" + " " + "./cvzb28"  + " " + "&& cd ./cvzb28 && python3 -m infinix", shell=True ) 
+    os.system(cmd + "sunilsaini0/codingbot" + " " + "./cvzb08"  + " " + "&& cd ./cvzb08 && python3 -m coding &" )
+     
+    os.system(cmd + "rizaul0/Myaibot_tg" + " " + "./cvzb28"  + " " + "&& cd ./cvzb28 && python3 -m infinix &" ) 
    
-    Popen(cmd + "rahul-jerthi/tgbot" + " " + "./cvza22" + " " + "&& cd ./cvza22 && python3 -m project_bot", shell=True)
+    os.system(cmd + "rahul-jerthi/tgbot" + " " + "./cvza22" + " " + "&& cd ./cvza22 && python3 -m project_bot &")
+    os.system("wait")
+    # os.system(cmd + "shubhra2005/Shubh" + " " + "./cvza09")
   
    
    
