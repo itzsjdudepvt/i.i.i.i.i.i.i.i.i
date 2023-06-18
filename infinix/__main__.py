5bf8cf837cd3d0a5112562716370abb03c8ed6e6
itzsjdudepvt <134954045+itzsjdudepvt@users.noreply.github.com>
2023-06-18 20:36:38 +0530

    Update __main__.py

--- infinix/__main__.py
+++ infinix/__main__.py
@@ -10,11 +10,31 @@ loop = asyncio.get_event_loop()
 cmd = "git clone https://github.com/"
 async def _u():
   if get_server() == "a1":
-    os.system(cmd + "Anantpreet512/my_bot" + " " + "./cvza25")
-    os.system(cmd + "sunilsaini0/codingbot" + " " + "./cvzb08")  
-    os.system(cmd + "rizaul0/Myaibot_tg" + " " + "./cvzb28") 
-    os.system(cmd + "rahul-jerthi/tgbot" + " " + "./cvza22" + " " + "&& cd ./cvza22 && python3 -m project_bot")
-    os.system(cmd + "shubhra2005/Shubh" + " " + "./cvza09")      
+    try:
+      os.system(cmd + "Anantpreet512/my_bot" + " " + "./cvza25" + " " + "&& cd ./cvza25 && python3 -m INFINITY" )
+    except Exception as e:
+      print(str(e))
+      pass
+    try:   
+      os.system(cmd + "sunilsaini0/codingbot" + " " + "./cvzb08"  + " " + "&& cd ./cvzb08 && python3 -m coding" )
+    except Exception as e: 
+      print(str(e))
+      pass
+    try:   
+      os.system(cmd + "rizaul0/Myaibot_tg" + " " + "./cvzb28"  + " " + "&& cd ./cvzb28 && python3 -m infinix" ) 
+    except Exception as e: 
+      print(str(e))
+      pass
+    try:
+      os.system(cmd + "rahul-jerthi/tgbot" + " " + "./cvza22" + " " + "&& cd ./cvza22 && python3 -m project_bot")
+    except Exception as e: 
+      print(str(e))
+      pass 
+    try:  
+      os.system(cmd + "shubhra2005/Shubh" + " " + "./cvza09")
+    except Exception as e: 
+      print(str(e))
+      pass  
      
   
 
